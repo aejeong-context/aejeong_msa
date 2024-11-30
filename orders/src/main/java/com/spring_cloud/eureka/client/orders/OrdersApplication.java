10739158e13929bdd297f037df649b6b2c87e5d6
aejeong <aejeong-context@kakao.com>
2024-11-30 22:40:21 +0900

    Initial commit

---
+++ orders/src/main/java/com/spring_cloud/eureka/client/orders/OrdersApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.eureka.client.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApplication.class, args);
+	}
+
+}
---
+++ users/src/test/java/com/spring_cloud/eureka/client/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.eureka.client.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
